fd0e3d8a2e02946a661455794a31638a2045fa32
Jusuf Muftic <jusuf.muftic@gmail.com>
2024-01-24 17:54:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelector('.tabs');
+    const panels = document.querySelectorAll('.panel');
+
+tabs.addEventListener('click', (e)=> {
+    if(e.target.tagName == "LI"){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel){
+            if (panel == targetPanel){
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active');
+            }
+        })  
+    }
+})
+
+
+
